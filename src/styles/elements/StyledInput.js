c5a7458138aa49587d672049b0fb85680dd779e7
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-03-14 18:57:09 -0300

    :lipstick: Fixing styled input component

--- src/styles/elements/StyledInput.js
+++ src/styles/elements/StyledInput.js
@@ -1,20 +1,21 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 const StyledInput = styled.input`
   padding: 8px;
   border: none;
   width: 100%;
   margin-top: 10px;
-  background-color: var(--blue-tertinary); 
-  border-bottom: 1px solid white;
+  background-color: var(--purple-primary); 
+  border-bottom: 1px solid var(--purple-border);
   letter-spacing: 1.4px;
   color: white;
+  transition: .2s border ease-in;
 
   &:focus {
     background-color: var(--blue-tertinary);
     border-bottom: 1px solid white;
     outline: none;
   }
-`
+`;
 
-export default StyledInput
+export default StyledInput;
