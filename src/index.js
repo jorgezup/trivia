b322b04886be89eef271c357a8ad7fdb454bd362
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-03-14 13:21:20 -0300

    :lipstick: Added global basic UI config

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './store/store';
+import Reset from './styles/global/Reset';
 
 ReactDOM.render(
   <Provider store={ store }>
+    <Reset />
     <App />
   </Provider>,
   document.getElementById('root'),
---
+++ src/styles/global/Reset.js
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from 'styled-components';
+
+const Reset = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :root {
+    --purple-primary: #6159C6;
+    --purple-secondary: #423C96;
+    --purple-tertiary: #1f1b52;
+    --purple-border: #A099FF;
+    --blue-primary: #2C2C9B;
+    --blue-secondary: #312FAC;
+    --yellow-primary: #FFBE55;
+    --yellow-secondary: #FFE05F;
+    --red-primary: #E02D5C;
+  }
+
+  body {
+    width: 100vw;
+    height: 100vh;
+    color: white;
+    font-size: 14px;
+    font-family: 'Fredoka One', cursive;
+    background-image: url('https://stopots.com/images/background.png');
+    background-repeat: no-repeat;
+    background-position: center;
+  }
+`;
+
+export default Reset;
