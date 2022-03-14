9e5faac4ee58388a8b0a630ccde28760a4338701
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-03-14 12:37:31 -0300

    :fire: Removed unecessary files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-// import logo from './trivia.png';
-import './App.css';
 import AppRoutes from './routes/index';
 
 export default function App() {
